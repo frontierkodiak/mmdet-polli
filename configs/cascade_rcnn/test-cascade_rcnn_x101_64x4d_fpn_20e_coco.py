9fb0d1b6ce5230a245c0b051a5d87a416daf2e5f
Caleb Sowers <frontierkodiak@gmail.com>
2023-02-16 15:53:12 -0500

    Update test-cascade_rcnn_x101_64x4d_fpn_20e_coco.py

--- configs/cascade_rcnn/test-cascade_rcnn_x101_64x4d_fpn_20e_coco.py
+++ configs/cascade_rcnn/test-cascade_rcnn_x101_64x4d_fpn_20e_coco.py
@@ -1,12 +1,14 @@
 _base_ = './cascade_rcnn_r50_fpn_20e_coco.py'
 
 dataset_type = 'CocoDataset'
-classes = '/local-data/Polli/Datasets/inaturalist2017/train_val/super_classes.txt'
+classes = ('Insecta','Chromista','Animalia','Plantae','Aves','Mollusca','Protozoa','Mammalia','Amphibia','Fungi','Reptilia','Actinopterygii', 'Arachnida')
 data = dict(
     train = dict(
-        img_prefix = '',
-        classes = classes,
-        ann_file = '/local-data/Polli/Datasets/inaturalist2017/train_val/train_2017_bboxes_super.json'),
+            dataset=dict(
+                img_prefix = '',
+                classes = classes,
+                ann_file = '/local-data/Polli/Datasets/inaturalist2017/train_val/train_2017_bboxes_super.json')
+    ),
     val = dict(
         img_prefix = '',
         classes = classes,
@@ -26,6 +28,20 @@ model = dict(
         norm_cfg=dict(type='BN', requires_grad=True),
         style='pytorch',
         init_cfg=dict(
-            type='Pretrained', checkpoint='open-mmlab://resnext101_64x4d')))
+            type='Pretrained', checkpoint='open-mmlab://resnext101_64x4d')),
+        roi_head=dict(
+        type='CascadeRoIHead',
+        num_stages=3,
+        stage_loss_weights=[1, 0.5, 0.25],
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=[
+            dict(type='Shared2FCBBoxHead', num_classes=13),
+            dict(type='Shared2FCBBoxHead', num_classes=13),
+            dict(type='Shared2FCBBoxHead', num_classes=13)
+        ]))
 
-# TODO: update config to reflect correct no. of classes
+load_from = '/models/pretrained/cascade_mask_rcnn_x101_64x4d_fpn_20e_coco_20200512_161033-bdb5126a.pth'
