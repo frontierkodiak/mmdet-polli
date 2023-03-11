6b18fcf408790b0288f2158c91d5ed231e5afd8b
Caleb Sowers <frontierkodiak@gmail.com>
2023-03-10 20:47:23 -0500

    Finished detect

--- tools/detect.py
+++ tools/detect.py
@@ -2,28 +2,48 @@ from PIL import Image
 from mmdet.apis import inference_detector, init_detector, show_result_pyplot
 from pathlib import Path
 import numpy as np
+from tqdm import tqdm
 
-
-input_dir = '/local-data/Polli/Datasets/backyard/birdfeeders/images'
+input_dir = '/lake/Polli/Datasets/imago_RMBL_2020'
+recursive = True
 ckpt_path = '/mmdetection/work_dirs/Mx1A1.E11.1tiny.6/epoch_104.pth'
 cfg_path = '/mmdetection/work_dirs/Mx1A1.E11.1tiny.6/Mx1A1.E11.1tiny.6.py'
 
-output_root = '/lake/Polli/Outputs/Mx1A1/E11.1tiny.6/backyard/birdfeeders/images'
+# output_root = '/local-data/Polli/Outputs/Mx1A1/E11.1tiny.6/backyard/birdfeeders/images' # ~3.5it/s. possibly bad comparison b/c different threshold
+output_root = '/local-data/Polli/Outputs/Mx1A1/E11.1tiny.6/imago_RMBL_2020'
 
-model = init_detector(cfg_path, ckpt_path, device='cuda:0')
-score_thr = 0.7
+model = init_detector(cfg_path, ckpt_path)#, device='cuda')
+score_thr = 0.8
+save_labels_conf = True
 classes = model.CLASSES
 
-# Find all images in the input directory. Don't search recursively.
-images = list(Path(input_dir).glob('*.jpg','*.png','*.jpeg','*.JPG','*.PNG','*.JPEG'))
+if not Path(output_root).exists():
+    Path(output_root).mkdir(parents=True)
 
+# Find all images in the input directory.
+### Images can have extensions: '*.jpg','*.png','*.jpeg','*.JPG','*.PNG','*.JPEG'
+if recursive:
+    images = [f for f in Path(input_dir).rglob('*.jpg')]
+    images.extend([f for f in Path(input_dir).rglob('*.png')])
+    images.extend([f for f in Path(input_dir).rglob('*.jpeg')])
+    images.extend([f for f in Path(input_dir).rglob('*.JPG')])
+    images.extend([f for f in Path(input_dir).rglob('*.PNG')])
+    images.extend([f for f in Path(input_dir).rglob('*.JPEG')])
+else:
+    images = [f for f in Path(input_dir).glob('*.jpg')]
+    images.extend([f for f in Path(input_dir).glob('*.png')])
+    images.extend([f for f in Path(input_dir).glob('*.jpeg')])
+    images.extend([f for f in Path(input_dir).glob('*.JPG')])
+    images.extend([f for f in Path(input_dir).glob('*.PNG')])
+    images.extend([f for f in Path(input_dir).glob('*.JPEG')])
+images = sorted(images)
 
-def cropDetection(imagePath, bboxes, labels_impt):
+def cropDetection(imagePath, bboxes):
     im = Image.open(imagePath)
-    left = bboxes[labels_impt][0][0]
-    top = bboxes[labels_impt][0][1]
-    right = bboxes[labels_impt][0][2]
-    bottom = bboxes[labels_impt][0][3]
+    left = int(bboxes[0])
+    top = int(bboxes[1])
+    right = int(bboxes[2])
+    bottom = int(bboxes[3])
     im1 = im.crop((left, top, right, bottom))
     return im1    
     
@@ -34,35 +54,43 @@ def postProcessResult(bbox_result, score_thr=0.5, classes=model.CLASSES):
     ]
     labels = np.concatenate(labels)
     bboxes = np.vstack(bbox_result)
-    labels_impt = np.where(bboxes[:, -1] > 0.3)[0]
+    labels_impt = np.where(bboxes[:, -1] > score_thr)[0]
+    labels_conf = bboxes[labels_impt, -1]
 
     labels_impt_list = [labels[i] for i in labels_impt]
     labels_class = [classes[i] for i in labels_impt_list]
     
-    return labels_impt, labels_class, bboxes
+    return labels_impt, labels_class, bboxes, labels_conf
 
-def infer(imagePath, score_thr=0.5, classes=model.CLASSES):
+def infer(imagePath, score_thr=0.5, classes=model.CLASSES): # TODO: Convert to async detector, or bump up batch size.
     bbox_result = inference_detector(model, imagePath)
-    labels_impt, labels_class, bboxes = postProcessResult(bbox_result, score_thr=score_thr, classes=classes)
-    return labels_impt, labels_class, bboxes
+    labels_impt, labels_class, bboxes, labels_conf = postProcessResult(bbox_result, score_thr=score_thr, classes=classes)
+    return labels_impt, labels_class, bboxes, labels_conf
 
-def makeOutputPath(imagePath, bboxes, output_root, labels_impt, labels_class):
+def makeOutputPath(imagePath, bboxes, output_root, labels_class, labels_conf, save_labels_conf):
     "Make output path for the image. Each image will be saved in a folder named after the class. If the class folder doesn't exist, it will be created."
     # Get the image name
     image_name = imagePath.name
     # Prefix image name with bbox coordinates and class
-    image_name = f'{bboxes[labels_impt][0][0]}_{bboxes[labels_impt][0][1]}_{bboxes[labels_impt][0][2]}_{bboxes[labels_impt][0][3]}_{labels_class[0]}_{image_name}'
+    if save_labels_conf:
+        image_name = f'{int(bboxes[0])}_{int(bboxes[1])}_{int(bboxes[2])}_{int(bboxes[3])}_{labels_class}_{round(float(labels_conf),2)}_{image_name}'
+    else:
+        image_name = f'{int(bboxes[0])}_{int(bboxes[1])}_{int(bboxes[2])}_{int(bboxes[3])}_{labels_class}_{image_name}'
     # Make output path
-    output_path = Path(output_root) / labels_class[0] / image_name
+    output_dir = Path(output_root) / labels_class
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True, exist_ok=True)
+    output_path = output_dir / image_name
     return output_path
 
-for imagePath in images:
-    labels_impt, labels_class, bboxes = infer(imagePath, score_thr=score_thr, classes=classes)
+for imagePath in tqdm(images):
+    labels_impt, labels_class, bboxes, labels_conf = infer(imagePath, score_thr=score_thr, classes=classes)
     if len(labels_impt) > 0:
         k = 0
         for i in range(len(labels_impt)):
-            output_path = makeOutputPath(imagePath, bboxes[k], output_root, labels_impt[k], labels_class[k])
+            output_path = makeOutputPath(imagePath, bboxes[k], output_root, str(labels_class[k]), labels_conf[k], save_labels_conf)
             output_path.parent.mkdir(parents=True, exist_ok=True)
-            im1 = cropDetection(imagePath, bboxes[k], labels_impt[k])
+            im1 = cropDetection(imagePath, bboxes[k])
+            #print(f'Saving {output_path}')
             im1.save(output_path)
             k += 1
